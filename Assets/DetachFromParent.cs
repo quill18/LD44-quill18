77f8e6c430d0104493c11fd566d003d92b0ac051
Jack Taylor <msec.nt@gmail.com>
2019-05-04 17:29:42 +0300

    Hotfix

--- Assets/DetachFromParent.cs
+++ Assets/DetachFromParent.cs
@@ -7,6 +7,6 @@ public class DetachFromParent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.SetParent(null);
+        this.transform.SetParent(null); // Become Batman
     }
 }
